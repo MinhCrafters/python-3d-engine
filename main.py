b64983b9eed5fb4b179c9ee0ef95e3d7c5126815
BuildTools <levan@DESKTOP-57OGPA2.(none)>
2022-10-06 13:31:50 +0700

    updated

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
 import moderngl
 import sys
+from model import *
+from camera import Camera
+from light import Light
+from mesh import Mesh
+from scene import Scene
 
 
 class GraphicsEngine:
     def __init__(self, window_size=(1600, 900)):
+
         pygame.init()
 
         self.WINDOW_SIZE = window_size
@@ -17,26 +23,50 @@ class GraphicsEngine:
         pygame.display.set_mode(
             self.WINDOW_SIZE, flags=pygame.OPENGL | pygame.DOUBLEBUF)
 
+        pygame.event.set_grab(True)
+        pygame.mouse.set_visible(False)
+
         self.ctx = moderngl.create_context()
 
+        self.ctx.enable(flags=moderngl.DEPTH_TEST | moderngl.CULL_FACE)
+
         self.clock = pygame.time.Clock()
+        self.time = 0
+        self.delta_time = 0
+
+        self.light = Light()
+
+        self.camera = Camera(self)
+
+        self.mesh = Mesh(self)
+
+        self.scene = Scene(self)
 
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                self.mesh.destroy()
                 pygame.quit()
                 sys.exit()
 
     def render(self):
+
         self.ctx.clear(color=(0.08, 0.16, 0.18))
+
+        self.scene.render()
 
         pygame.display.flip()
 
+    def get_time(self):
+        self.time = pygame.time.get_ticks() * 0.001
+
     def run(self):
         while True:
+            self.get_time()
             self.check_events()
+            self.camera.update()
             self.render()
-            self.clock.tick(60)
+            self.delta_time = self.clock.tick(60)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,35 +1,89 @@
+import moderngl as mgl
 import numpy as np
+import glm
 
 
-class Triangle:
-    def __init__(self, app):
+class BaseModel:
+    def __init__(self, app, vao_name, tex_id, pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
         self.app = app
-        self.ctx = app.ctx
-        self.vbo = self.get_vbo()
-        self.shader_program = self.get_shader_program('default')
-        self.vao = self.get_vao()
+        self.pos = pos
+        self.rot = glm.vec3([glm.radians(a) for a in rot])
+        self.scale = scale
+        self.m_model = self.get_model_matrix()
+        self.tex_id = tex_id
+        self.vao = app.mesh.vao.vaos[vao_name]
+        self.program = self.vao.program
+        self.camera = self.app.camera
 
-    def get_vao(self):
-        vao = self.ctx.vertex_array(self.shader_program, [(self.vbo, '3f', 'in_position')])
-        return vao
+    def update(self): ...
 
-    def get_vertex_data(self):
-        vertex_data = [(-0.6, -0.8, 0.0), (0.6, -0.8, 0.0), (0.0, 0.8, 0.0)]
-        vertex_data = np.array(vertex_data, dtype='f4')
-        return vertex_data
+    def get_model_matrix(self):
+        m_model = glm.mat4()
 
-    def get_vbo(self):
-        vertex_data = self.get_vertex_data()
-        vbo = self.ctx.buffer(vertex_data)
-        return vbo
+        m_model = glm.translate(m_model, self.pos)
 
-    def get_shader_program(self, shader_name):
-        with open(f'shaders/{shader_name}.vert') as file:
-            vertex_shader = file.read()
+        m_model = glm.rotate(m_model, self.rot.z, glm.vec3(0, 0, 1))
+        m_model = glm.rotate(m_model, self.rot.y, glm.vec3(0, 1, 0))
+        m_model = glm.rotate(m_model, self.rot.x, glm.vec3(1, 0, 0))
 
-        with open(f'shaders/{shader_name}.frag') as file:
-            fragment_shader = file.read()
+        m_model = glm.scale(m_model, self.scale)
+        return m_model
 
-        program = self.ctx.program(vertex_shader=vertex_shader, fragment_shader=fragment_shader)
+    def render(self):
+        self.update()
+        self.vao.render()
 
-        return program
+
+class Cube(BaseModel):
+    def __init__(self, app, vao_name='cube', tex_id=0, pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
+        super().__init__(app, vao_name, tex_id, pos, rot, scale)
+        self.on_init()
+
+    def update(self):
+        self.texture.use()
+        self.program['camPos'].write(self.camera.position)
+        self.program['m_view'].write(self.camera.m_view)
+        self.program['m_model'].write(self.m_model)
+
+    def on_init(self):
+
+        self.texture = self.app.mesh.texture.textures[self.tex_id]
+        self.program['u_texture_0'] = 0
+        self.texture.use()
+
+        self.program['m_proj'].write(self.camera.m_proj)
+        self.program['m_view'].write(self.camera.m_view)
+        self.program['m_model'].write(self.m_model)
+
+        self.program['light.position'].write(self.app.light.position)
+        self.program['light.Ia'].write(self.app.light.Ia)
+        self.program['light.Id'].write(self.app.light.Id)
+        self.program['light.Is'].write(self.app.light.Is)
+
+
+class Cat(BaseModel):
+    def __init__(self, app, vao_name='cat', tex_id='cat',
+                 pos=(0, 0, 0), rot=(-90, 0, 0), scale=(1, 1, 1)):
+        super().__init__(app, vao_name, tex_id, pos, rot, scale)
+        self.on_init()
+
+    def update(self):
+        self.texture.use()
+        self.program['camPos'].write(self.camera.position)
+        self.program['m_view'].write(self.camera.m_view)
+        self.program['m_model'].write(self.m_model)
+
+    def on_init(self):
+
+        self.texture = self.app.mesh.texture.textures[self.tex_id]
+        self.program['u_texture_0'] = 0
+        self.texture.use()
+
+        self.program['m_proj'].write(self.camera.m_proj)
+        self.program['m_view'].write(self.camera.m_view)
+        self.program['m_model'].write(self.m_model)
+
+        self.program['light.position'].write(self.app.light.position)
+        self.program['light.Ia'].write(self.app.light.Ia)
+        self.program['light.Id'].write(self.app.light.Id)
+        self.program['light.Is'].write(self.app.light.Is)
